34cf3c5a9f390dd6411d4ebb6b5ce9b1066a37cd
TalisTalis <tsarst@gmail.com>
2023-01-14 14:19:57 +0300

    complete

--- CodeBlog.Cycles/Program.cs
+++ CodeBlog.Cycles/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeBlog.Cycles
 {
@@ -10,6 +6,60 @@ namespace CodeBlog.Cycles
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[15];
+
+            int i = 0;
+
+            while (i < arr.Length)
+            {
+                Console.Write($"Введите {i} элемент массива: ");
+
+                if (int.TryParse(Console.ReadLine(), out int res))
+                {
+                    arr[i] = res;
+
+                    i++;
+                }
+            }
+
+            int sumElementArray = 0;
+
+            for (int j = 0; j < arr.Length; j++)
+            {
+                sumElementArray += arr[j];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Сумма элементов массива равна {sumElementArray}");
+            Console.WriteLine();
+
+            int diffElementsArray = arr[0];
+            int k = 1;
+
+            while (k < arr.Length)
+            {
+                diffElementsArray -= arr[k];
+                k++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Разность элементов массива равна {diffElementsArray}");
+            Console.WriteLine();
+
+            int multElementsArray = arr[0];
+
+            int l = 1;
+
+            do
+            {
+                multElementsArray *= arr[l];
+                l++;
+
+            } while (l < arr.Length);
+
+            Console.WriteLine();
+            Console.WriteLine($"Произведение элементов массива равна {multElementsArray}");
+            Console.WriteLine();
         }
     }
 }
